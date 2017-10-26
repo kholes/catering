ef12b847379946271f0fe9f7c1c020f2c082b810
Rizki Joelisman <email": null>
2017-10-26 19:40:50 +0700

    db atlas has connect

--- server/app.js
+++ server/app.js
@@ -3,8 +3,32 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors')
 const app = express();
+const mongoose = require('mongoose')
+
+//db mongoo Atlas
+var MongoClient = require('mongodb').MongoClient;
+var uri = "mongodb://admin123:admin123@cluster0-shard-00-00-cquc7.mongodb.net:27017,cluster0-shard-00-01-cquc7.mongodb.net:27017,cluster0-shard-00-02-cquc7.mongodb.net:27017/catering?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+MongoClient.connect(uri, function(err, db) {
+  db.close();
+});
+
+
+app.get('/', (req,res)=>{
+    res.json('mongoose connected')
+})
+
+
+
+
+app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.json({type: 'application/x-www-form-urlencoded'}))
+app.use(cors())
+
+
+
+
 
 
 app.use((req,res) => {
