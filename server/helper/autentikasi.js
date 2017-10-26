89b84b038d50900277c60ca71111ac02c3564bbd
Rizki Joelisman <email": null>
2017-10-26 22:00:00 +0700

    update helper autentikasi

---
+++ server/helper/autentikasi.js
@@ -0,0 +1,23 @@
+var jwt =require('jsonwebtoken');
+require('dotenv').config();
+
+function authAdmin(req,res,next) {
+    var token = req.headers.token
+    console.log(token);
+    console.log(req.headers.token);
+    if (token) {
+      jwt.verify(token,process.env.SECRET_KEY,(err,decoded)=> {
+        if (decoded.role == 'admin') {
+          return next()
+        }else {
+          res.status(500).send('not authorized')
+        }
+      })
+    }
+    else {
+      res.send("404 Not Found")
+    }
+
+ }
+
+module.exports ={authAdmin} ;
